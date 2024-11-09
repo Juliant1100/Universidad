179e39580b99f9e22a93e5231eaf08600accee78
Juliant1100 <juanperez@example.com>
2024-11-08 22:50:37 -0500

    Inicializar proyecto con HTML y CSS

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('cambiarTexto').addEventListener('click', function() {
+    document.getElementById('programa').innerHTML = '<h2>Programa Actualizado</h2><p>Nueva información del programa.</p>';
+});
